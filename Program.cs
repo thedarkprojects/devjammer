acf313d888e2339b9869c4da8593b8923d852d10
Azeez Adewale <azeezadewale98@gmail.com>
2019-12-01 19:34:34 +0100

    make code faster and efficient for quick jamming

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Management;
+using System.Threading;
 
 namespace devjammer
 {
@@ -34,37 +35,95 @@ namespace devjammer
             }
             else
             {
-                Console.WriteLine("Error: invalid first argument ");
+                Console.WriteLine("Error: invalid first argument '" + arg + "'");
                 Console.WriteLine(GetHelp());
                 Environment.Exit(1);
             }
-            var usbDevices = GetLogicalDevices();
+            String query = String.Format(@"Select * From CIM_LogicalDevice WHERE DeviceID LIKE '%{0}%' OR Caption LIKE '%{0}%' OR Description LIKE '%{0}%'", extraParam);
+            var usbDevices = GetLogicalDevices(query);
             foreach (var usbDevice in usbDevices)
             {
-                if (usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam) ||
-                    usbDevice.GetPropertyValue("Caption").ToString().ToLower().Contains(extraParam) ||
-                    usbDevice.GetPropertyValue("Description").ToString().ToLower().Contains(extraParam) ||
-                    usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam))
+                try
                 {
-                    /**Console.WriteLine("Name: {0}, Status: {1}, Instance Path: {2}",
-                       usbDevice.GetPropertyValue("Caption"),
-                       usbDevice.GetPropertyValue("Status"),
-                       usbDevice.GetPropertyValue("DeviceID"));
+                    if (usbDevice.GetPropertyValue("DeviceID").ToString().StartsWith("\\\\") ||
+                        usbDevice.GetPropertyValue("DeviceID").ToString().StartsWith("//"))
+                    {
+                        continue;
+                    }
+                    if (usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam) ||
+                        usbDevice.GetPropertyValue("Caption").ToString().ToLower().Contains(extraParam) ||
+                        usbDevice.GetPropertyValue("Description").ToString().ToLower().Contains(extraParam) ||
+                        usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam))
+                    {
+                        Guid mouseGuid = new Guid(usbDevice.GetPropertyValue("ClassGuid").ToString());
+                        string instancePath = usbDevice.GetPropertyValue("DeviceID").ToString();
+                        if (arg.Equals("enable"))
+                        {
+                            try
+                            {
+                                DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
+                            } catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                            }
+                        }
+                        else if (arg.Equals("disable"))
+                        {
+                            try
+                            {
+                                DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, false);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                            }
+                        }
+                        else if (arg.Equals("jam"))
+                        {
+                            if (usbDevice.GetPropertyValue("Status").ToString().ToUpper().Equals("OK"))
+                            {
+                                continue;
+                            }
+                            try
+                            {
+                                Thread thread1 = new Thread(delegate ()
+                                {
+                                    while (true) DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
+                                });
+                                Thread thread2 = new Thread(delegate ()
+                                {
+                                    do
+                                    {
+                                        Thread.Sleep(30000);//we need to sleep here so we can wait enough to check if the device is disabled again
+                                    } while (!GetLogicalDevices(String.Format("{0} AND NOT DeviceID LIKE '\\\\%' AND NOT DeviceID LIKE '//%' AND Status='Error'", query))[0].GetPropertyValue("Status").ToString().ToUpper().Equals("OK"));
+                                    thread1.Abort();
+                                });
+                                thread1.Start();
+                                thread2.Start();
+                                thread1.Join();
+                                thread2.Join();
 
-                    Guid mouseGuid = new Guid(usbDevice.GetPropertyValue("ClassGuid").ToString());
-                    string instancePath = usbDevice.GetPropertyValue("DeviceID").ToString();**/
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                            }
+                        }
+                        Console.WriteLine("{0}ed {1}-{2}",
+                           arg,
+                           usbDevice.GetPropertyValue("Caption"),
+                           usbDevice.GetPropertyValue("DeviceID"));
+                    }
+                } catch (Exception ex)
+                {
 
-                    //while (true) DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
-                    //Console.WriteLine("Done");
                 }
             }
-
-            Console.Read();
         }
 
         static void PrintAllLogicalDevice(String extraParam)
         {
-            var usbDevices = GetLogicalDevices();
+            var usbDevices = GetLogicalDevices(String.Format(@"Select * From CIM_LogicalDevice WHERE DeviceID LIKE '%{0}%' OR Caption LIKE '%{0}%' OR Description LIKE '%{0}%'", extraParam));
             foreach (var usbDevice in usbDevices)
             {
                 try
@@ -85,12 +144,12 @@ namespace devjammer
             }
         }
 
-        static List<ManagementBaseObject> GetLogicalDevices()
+        static List<ManagementBaseObject> GetLogicalDevices(string query = @"Select * From CIM_LogicalDevice")
         {
             List<ManagementBaseObject> devices = new List<ManagementBaseObject>();
 
             ManagementObjectCollection collection;
-            using (var searcher = new ManagementObjectSearcher("root\\CIMV2", @"Select * From CIM_LogicalDevice"))
+            using (var searcher = new ManagementObjectSearcher("root\\CIMV2", query))
                 collection = searcher.Get();
 
             foreach (var device in collection)
