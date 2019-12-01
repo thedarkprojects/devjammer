43a670979d1b4aacdf6bf7a3ca4968a38c14f185
Azeez Adewale <azeezadewale98@gmail.com>
2019-12-01 16:12:34 +0100

    change project name from driverjammer to devjammer

--- Program.cs
+++ Program.cs
@@ -3,27 +3,56 @@ using System;
 using System.Collections.Generic;
 using System.Management;
 
-namespace driverjammer
+namespace devjammer
 {
     //see https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/cim-logicaldevice
     class Program
     {
         static void Main(string[] args)
         {
-            String name = "USBSTOR";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: You need to specify an argument ");
+                Console.WriteLine(GetHelp());
+                Environment.Exit(1);
+            }
+            String arg = args[0].ToLower();
+            String extraParam = (args.Length > 1 ? args[1].ToLower() : "");
+            if (arg.Equals("help"))
+            {
+                Console.WriteLine(GetHelp());
+                Environment.Exit(0);
+            }
+            else if (arg.Equals("list"))
+            {
+                PrintAllLogicalDevice(extraParam);
+                Environment.Exit(0);
+            }
+            else if (arg.Equals("enable") || arg.Equals("disable") || arg.Equals("jam"))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Error: invalid first argument ");
+                Console.WriteLine(GetHelp());
+                Environment.Exit(1);
+            }
             var usbDevices = GetLogicalDevices();
             foreach (var usbDevice in usbDevices)
             {
-                if (usbDevice.GetPropertyValue("DeviceID").ToString().Contains(name))
+                if (usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam) ||
+                    usbDevice.GetPropertyValue("Caption").ToString().ToLower().Contains(extraParam) ||
+                    usbDevice.GetPropertyValue("Description").ToString().ToLower().Contains(extraParam) ||
+                    usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam))
                 {
-                    Console.WriteLine("Device ID: {0}, PNP Device ID: {1}, Description: {2}, Status: {3}",
-                        usbDevice.GetPropertyValue("DeviceID"), usbDevice.GetPropertyValue("SystemCreationClassName"), usbDevice.GetPropertyValue("Description"), usbDevice.GetPropertyValue("Status"));
-                    
-                    // every type of device has a hard-coded GUID, this is the one for mice
-                    Guid mouseGuid = new Guid("{4d36e967-e325-11ce-bfc1-08002be10318}");
+                    /**Console.WriteLine("Name: {0}, Status: {1}, Instance Path: {2}",
+                       usbDevice.GetPropertyValue("Caption"),
+                       usbDevice.GetPropertyValue("Status"),
+                       usbDevice.GetPropertyValue("DeviceID"));
 
-                    // get this from the properties dialog box of this device in Device Manager
-                    string instancePath = @usbDevice.GetPropertyValue("DeviceID").ToString();
+                    Guid mouseGuid = new Guid(usbDevice.GetPropertyValue("ClassGuid").ToString());
+                    string instancePath = usbDevice.GetPropertyValue("DeviceID").ToString();**/
 
                     //while (true) DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
                     //Console.WriteLine("Done");
@@ -31,6 +60,29 @@ namespace driverjammer
             }
 
             Console.Read();
+        }
+
+        static void PrintAllLogicalDevice(String extraParam)
+        {
+            var usbDevices = GetLogicalDevices();
+            foreach (var usbDevice in usbDevices)
+            {
+                try
+                {
+                    if (usbDevice.GetPropertyValue("DeviceID").ToString().ToLower().Contains(extraParam) ||
+                        usbDevice.GetPropertyValue("Caption").ToString().ToLower().Contains(extraParam) ||
+                        usbDevice.GetPropertyValue("Description").ToString().ToLower().Contains(extraParam))
+                    {
+                        Console.WriteLine("Name: {0}, Status: {1}, Instance Path: {2}",
+                           usbDevice.GetPropertyValue("Caption"),
+                           usbDevice.GetPropertyValue("Status"),
+                           usbDevice.GetPropertyValue("DeviceID"));
+                    }
+                } catch (Exception ex)
+                {
+
+                }
+            }
         }
 
         static List<ManagementBaseObject> GetLogicalDevices()
@@ -49,6 +101,19 @@ namespace driverjammer
             collection.Dispose();
             return devices;
         }
+
+        static String GetHelp()
+        {
+            return "devjammer [[help],[list],[enable],[disable],[jam]] \"device part\"" +
+                "\ndevjammer list \"WD Elements\"" +
+                "\n" +
+                "\nhelp      show this help message" +
+                "\nlist      list all devices" +
+                "\nenable    enable a device that match the second argument" +
+                "\ndisable   disable a device that match the second argument" +
+                "\njam       keep enabling a device until system deny access(shady)";
+        }
+
     }
 
 }
